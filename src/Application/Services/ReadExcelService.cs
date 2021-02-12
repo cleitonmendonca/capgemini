d325cc49dc8887e9165059bafccd09f62ad80429
Cleiton da Silva Mendonca <cleiton.mendonca@gmail.com>
2021-02-12 09:13:15 -0400

    Criado o serviço para executar a leitura do arquivo em excel

---
+++ src/Application/Services/ReadExcelService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Commands;
+using Application.Core.Behaviors;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using OfficeOpenXml;
+
+namespace Application.Services
+{
+    public class ReadExcelService : IReadExcelService
+    {
+        private readonly IFormFile _file;
+
+        public ReadExcelService(IFormFile file)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            _file = file;
+        }
+
+        public async Task<Response> OpenExcelFile()
+        {
+            var stream = new MemoryStream();
+            await _file.CopyToAsync(stream).ConfigureAwait(false);
+
+            var package = new ExcelPackage(stream);
+
+            var worksheet = package.Workbook.Worksheets[0];
+            return await ReadExcelPackage(worksheet);
+        }
+
+        private async Task<Response> ReadExcelPackage(ExcelWorksheet sheet)
+        {
+            var response = new Response();
+            var rows = sheet.Dimension?.Rows;
+            var cols = sheet.Dimension?.Columns;
+
+            if (Equals(!rows.HasValue) || Equals(!cols.HasValue))
+            {
+                response.AddError("O Arquivo enviado não possui dados para serem lidos!");
+                return response;
+            }
+            
+            if (cols > 4)
+            {
+                response.AddError("O Layout do Arquivo informado é inválido, o sistema só permite leitura de arquivo com 04 (quatro) colunas.");
+                return response;
+            }
+
+            IList<CreateProductCommand> documentCommandsList = new List<CreateProductCommand>();
+            for (int row = 2; row <= rows; row++)
+            {
+                var createDocument = new CreateProductCommand();
+                for (int col = 1; col <= cols; col++)
+                {
+                    var cell = sheet.Cells[row, col].Value;
+                    var position = sheet.Cells[row, col].Address;
+
+                    switch (col)
+                    {
+                        case 1:
+                            ValidatorDataExcel.ValidateDeliveryDate(col, cell, createDocument, response, position);
+                            break;
+                        case 2:
+                            ValidatorDataExcel.ValidateDescription(col, cell, createDocument, response, position);
+                            break;
+                        case 3:
+                            ValidatorDataExcel.ValidateQuantity(col, cell, createDocument, response, position);
+                            break;
+                        case 4:
+                            ValidatorDataExcel.ValidateValue(col, cell, createDocument, response, position);
+                            break;
+                    }
+                }
+                documentCommandsList.Add(createDocument);
+            }
+
+            if (!response.Errors.Any())
+            {
+                response = new Response(documentCommandsList);
+            }
+            return response;
+        }
+
+    }
+}
---
+++ src/Application/Services/ValidatorDataExcel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Application.Commands;
+using Application.Core.Behaviors;
+
+namespace Application.Services
+{
+    public static class ValidatorDataExcel
+    {
+        public static void ValidateValue(int col, object cell, CreateProductCommand createProduct, Response response,
+            object position)
+        {
+            if (cell != null && (double) cell > 0.00)
+            {
+                createProduct.Value = Decimal.Parse(cell.ToString());
+            }
+            else
+            {
+                response.AddError(
+                    $"O Valor do produto informado na celula {position} é inválido! O valor do produto deve ser superior a 0.");
+            }
+        }
+
+        public static void ValidateQuantity(int col, object cell, CreateProductCommand createProduct,
+            Response response,
+            object position)
+        {
+            if (cell != null && (double) cell > 0)
+            {
+                createProduct.Quantity = (double) cell;
+            }
+            else
+            {
+                response.AddError(
+                    $"O Valor quantidade informada na celula {position} é inválida! A quantidade do produto deve ser superior a 0.");
+            }
+        }
+
+        public static void ValidateDescription(int col, object cell, CreateProductCommand createProduct,
+            Response response,
+            object position)
+        {
+            if (cell!= null && !string.IsNullOrEmpty(cell.ToString()) && !string.IsNullOrWhiteSpace(cell.ToString()) && cell.ToString().Length < 51)
+            {
+                createProduct.Name = cell.ToString();
+            }
+            else
+            {
+                response.AddError(
+                    $"O Valor da descrição informada na celula {position} é inválida! A quantiade máxima de caracter permitido é 50");
+            }
+        }
+
+        public static void ValidateDeliveryDate(int col, object cell, CreateProductCommand createProduct,
+            Response response,
+            object position)
+        {
+            if (cell != null && !string.IsNullOrEmpty(cell.ToString()) && !string.IsNullOrWhiteSpace(cell.ToString()))
+            {
+                createProduct.DeliveredOn = DateTime.Parse(cell.ToString());
+            }
+            else
+            {
+                response.AddError(
+                    $"O Valor da data de entrega informada na celula {position} é inválida! A data de entrega tem que ser superior a data atual");
+            }
+        }
+
+        public static void ValidateContentType(string contentType, Response response)
+        {
+            if (!contentType.Equals("application/vnd.ms-excel") &&
+                !contentType.Equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+            {
+                response.AddError("O Formato de arquivo informado é inválido");
+            }
+        }
+    }
+}
