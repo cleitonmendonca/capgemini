303ad422dccf986b1b7f7bb6ede81657451f8c9e
Cleiton da Silva Mendonca <cleiton.mendonca@gmail.com>
2021-02-12 09:19:00 -0400

    Criado as entidades que irão persistir os dados no banco de dados juntamentos com as classes de mapeamento e de view models

---
+++ src/Application/Entities/Importation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Application.Core;
+
+namespace Application.Entities
+{
+    public class Importation : BaseEntity
+    {
+        public DateTime LessDeliveredDate { get; private set; }
+        public ICollection<Product> Products { get; private set; }
+        public Importation(DateTime lessDeliveredDate)
+        {
+            LessDeliveredDate = lessDeliveredDate;
+        }
+
+        public void SetLowerDelivererDate()
+        {
+            var deleiveredOn = Products?.OrderByDescending(pa => pa.DeliveredOn)?.FirstOrDefault()?.DeliveredOn;
+            if (deleiveredOn != null)
+                this.LessDeliveredDate =
+                    (DateTime) deleiveredOn;
+        }
+    }
+}
---
+++ src/Application/ViewModels/ImportationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Application.ViewModels
+{
+    public class ImportationViewModel
+    {
+        public int Id { get; set; }
+        public double TotalItems { get; set; }
+        public double TotalValue { get ; set; }
+        public DateTime LessDeliveredDate { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
