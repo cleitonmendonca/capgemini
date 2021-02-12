7efd036e4b96d6268e97c2bf948d9b5f8999f8f5
Cleiton da Silva Mendonca <cleiton.mendonca@gmail.com>
2021-02-12 09:24:43 -0400

    Criado o comando para passar os dados dos produtos que serão inserido no banco de dados e o seu validador

---
+++ src/Application/Commands/CreateProductCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using Application.Core.Behaviors;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Commands
+{
+    public class CreateProductCommand : IRequest<Response>
+    {
+        public DateTime DeliveredOn { set; get ; }
+        public string Name { get;  set; }
+        public double Quantity { get; set; }
+        public decimal Value { get; set; }
+    }
+}
