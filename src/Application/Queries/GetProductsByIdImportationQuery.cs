8e08127abb1288da509a8a11537849d02d75cbeb
Cleiton da Silva Mendonca <cleiton.mendonca@gmail.com>
2021-02-12 09:27:40 -0400

    Criado o CommandQuery para buscar todos os produtos importado pelo id da importação

---
+++ src/Application/Queries/GetProductsByIdImportationQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.Core.Behaviors;
+using MediatR;
+
+namespace Application.Queries
+{
+    public class GetProductsByIdImportationQuery : IRequest<Response>
+    {
+        public int ImportationId { get; set; }
+    }
+}
