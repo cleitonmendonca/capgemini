ab300bd5d0af26a35e9ee1824e3f4003db9f7a2f
Cleiton da Silva Mendonca <cleiton.mendonca@gmail.com>
2021-02-12 09:19:38 -0400

    Criado as migrations do sistema

---
+++ src/Infrastruture/Data/Migrations/20210212103456_AlterDeliveredPropertyInProduct.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastruture.Data.Migrations
+{
+    public partial class AlterDeliveredPropertyInProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DeleiveredOn",
+                table: "Products",
+                newName: "DeliveredOn");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DeliveredOn",
+                table: "Products",
+                newName: "DeleiveredOn");
+        }
+    }
+}
